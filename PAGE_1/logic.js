8c82e74b25934b3d51e4e9a0e0db17f63c57bce0
NOTYOUONI1 <sorry56363472@gmail.com>
2024-03-28 14:34:38 +0600

    march 28

---
+++ PAGE_1/logic.js
@@ -0,0 +1,58 @@
+/*class rolls{
+    constructor(){
+        console.log('parent')
+       // this.car = 'rolls'
+        this.name = 'rakib'
+
+    }
+    slep(){
+        console.log('niglt')
+    }
+    eat(){
+        console.log('rice')
+    }
+}
+
+class eng extends rolls{
+    constructor(mk,name){
+        console.log('child')
+        super(name);
+        this.ml = mk
+        this.work = 'coing'
+        console.log('exit')
+    }
+    workf(){
+        supper.eat();
+        console.log('work f')
+    }
+}
+
+let engobj = new eng('app')*/
+
+/*let data = 'sicret data'
+
+class usr {
+    constructor(name,email){
+        this.name = name
+        this.email = email
+    }
+    viwe_data(){
+        console.log('data')
+    }
+}
+
+class admin extends usr{
+    constructor(name,email){
+        super(name,email);
+    }
+
+    editedata(){
+        data =
+    }
+}
+
+let student1 = new usr('rakib','sorry')
+let student2 = new usr('sakib','mar56969')
+
+let admin1 = new admin('admin','admin@admin')*/
+

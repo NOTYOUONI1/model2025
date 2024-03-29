3cf0de62a5f1651d5906d0aaf25adda9f79d5e44
NOTYOUONI1 <sorry56363472@gmail.com>
2024-03-29 20:14:56 +0600

    march-29

--- PAGE_1/logic.js
+++ PAGE_1/logic.js
@@ -55,10 +55,106 @@ let student1 = new usr('rakib','sorry')
 let student2 = new usr('sakib','mar56969')
 
 let admin1 = new admin('admin','admin@admin')*/
-function hello(){
+/*function hello(){
     console.log('hello')
 }
 
 setTimeout(()=>{
     console.log('h')
-},2000)
+},2000)*/
+
+/*function getdata(dataid){
+    setTimeout(()=>{
+        console.log('data ID',dataid)
+    },2000)
+}*/
+/*let getpromise =()=>{
+    return new Promise((rv,rj)=>{
+        console.log('i ma a promise');
+        rj('network error')
+    })
+} 
+
+let gpromise = getpromise()
+
+gpromise.then((res)=>{
+    console.log('fullfild promise',res)
+});
+gpromise.catch((rej)=>{
+    console.log('promise fullfild but error',rej)
+})*/
+
+
+/*function asyncfunc(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('some data1')
+            resolve('success')
+        },4000)
+    })
+}
+function asyncfunc2(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('some data2')
+            resolve('success')
+        },4000)
+    })
+}
+
+function asyncfunc3(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('some data3')
+            resolve('success')
+        },4000)
+    })
+}
+
+asyncfunc()
+    .then(()=>{
+        return asyncfunc2()
+    })
+    .then((res)=>{
+        return  asyncfunc3()
+    })*/
+
+/*function promise(dataid){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('data id',dataid)
+            resolve('success')
+        },100)
+    })
+}
+
+(async function(){
+    console.log('data load...')
+    await promise(1);
+    console.log('data load...')
+    await promise(2);
+    console.log('data load...')
+    await promise(3);
+    console.log('data load...')
+    await promise(4);
+    console.log('successfull')
+})()
+try{
+on()
+} catch(err){
+    console.log(err)
+}*/
+
+const url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+const button = document.querySelector("#bin")
+const prapa = document.querySelector("#p")
+
+const getfect = async ()=>{
+    console.log('getting data...');
+    let responce = await fetch(url);
+    //let data = await responce.json();
+    prapa.innerText = responce
+
+};
+button.addEventListener("click",getfect)
